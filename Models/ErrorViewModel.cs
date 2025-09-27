9807b66ca01fba804140f3d623928470790a7039
Patrick Ponce <patrick_ponce1@usmp.pe>
2025-09-26 20:28:29 -0500

    Rama_Principal

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PC2_PatrickPonce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
