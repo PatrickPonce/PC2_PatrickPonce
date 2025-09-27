dbff41f4fb278208b9cfec95157e000f80ef9802
Patrick Ponce <patrick_ponce1@usmp.pe>
2025-09-26 20:47:30 -0500

    Actualizar ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PC2_PatrickPonce.Data;
 
-public class ApplicationDbContext : IdentityDbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
+public class ApplicationDbContext : IdentityDbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) {
     }
+    public DbSet<Inmueble> Inmuebles { get; set; }
+    public DbSet<Visita> Visitas { get; set; }
+    public DbSet<Reserva> Reservas { get; set; }
 }
